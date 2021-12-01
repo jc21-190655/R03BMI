73effc8e7bb9938c6bec8b74c742ad536880ea7b
悠 道輪 <190655@jc-21.jp>
2021-12-01 16:08:52 +0900

    完成

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -20,6 +20,8 @@ namespace R03BMI
         {
            var height = heighit.Text;
            var weight = weighit.Text;
+
+           string hantei = null;
            try
             {
                 double h = double.Parse(height);
@@ -28,15 +30,33 @@ namespace R03BMI
                 {
                     h = h / 100;
                 }
-                double bmi = w / (h * h);
-                double bmi2 = Math.Round(bmi,2);
+                if (w > 1000)
+                {
+                    w = w / 1000;
+                }
 
-                result.Text = "身長" + h + "体重" + w +"kgの人のBMI" +bmi2;
+                double bmi = w / (h * h);
+                double bmi2 = Math.Round(bmi,1);
+
+
+              if (bmi2 < 18.5) {
+	        	hantei="低体重(痩せ)";
+	        }else if(bmi2 < 25.0) {
+	    	  hantei="普通体重";
+	        }else if(bmi2 < 30.0) {
+	    	  hantei="肥満(1)";
+	        }else if(bmi2 < 35.0) { 
+              hantei = "肥満(2)";
+	        }else if(bmi2 < 40.0) {
+              hantei = "肥満(3)";
+            }else 
+              hantei = "肥満(4)";
+                result.Text = "身長" + h + "体重" + w +"kgの人のBMI" +bmi2 +hantei;
             }
             catch (FormatException ex)
             {
                 Console.WriteLine(ex);
-                result.Text = "!!!";
+                result.Text = "数字がありません";
             }
         }
     }
